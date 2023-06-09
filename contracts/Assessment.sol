5fd14ca055346fde65c87a018aef9c20eb294f73
Tanisha Ibrahim <131354740+tanishaibrahim@users.noreply.github.com>
2023-06-09 23:34:08 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
 
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
-
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+   
+    uint256 public sum;
 
     constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+        
+        sum = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    function getTotal() public view returns(uint256){
+        return sum;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+   
+    function calculateFactorial(uint n) public {
+        uint factorial = 1;
 
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
-    }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+        for (uint i = 1; i <= n; i++) {
+            factorial *= i;
         }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        sum= factorial;
     }
 }
