6113a5d9800ab8bab920c631b3114e83161e05fc
Tanisha Ibrahim <131354740+tanishaibrahim@users.noreply.github.com>
2023-06-09 23:16:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,25 +55,19 @@ export default function HomePage() {
 
   const getBalance = async() => {
     if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
+      setBalance((await atm.getTotal()).toNumber());
     }
   }
 
-  const deposit = async() => {
+  const factorial = async() => {
     if (atm) {
-      let tx = await atm.deposit(1);
+      let tx = await atm.calculateFactorial(5);
       await tx.wait()
       getBalance();
     }
   }
 
-  const withdraw = async() => {
-    if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
-      getBalance();
-    }
-  }
+  
 
   const initUser = () => {
     // Check to see if user has Metamask
@@ -92,10 +86,10 @@ export default function HomePage() {
 
     return (
       <div>
-        <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <p>My Account: {account}</p>
+        <p>Factorial: {balance}</p>
+        <button onClick={factorial}>Factorial</button>
+        
       </div>
     )
   }
@@ -104,11 +98,14 @@ export default function HomePage() {
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      <header><h1>Factorial of a Number</h1></header>
       {initUser()}
       <style jsx>{`
         .container {
-          text-align: center
+          text-align: center;
+          background-color: grey;
+          border: 8px dotted pink;
+          color: white;
         }
       `}
       </style>
